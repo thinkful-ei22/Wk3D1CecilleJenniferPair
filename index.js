5748024898f3327a7e8ed305c7496629bea15982
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-25 12:09:19 -0500

    morning exercises complete

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 // First GET Exercise Glitch Link: https://glitch.com/edit/#!/defiant-hammer?path=server.js:6:20
+
+//Second POST Exercise Glitch Link: https://glitch.com/edit/#!/inconclusive-wealth?path=server.js:26:0
